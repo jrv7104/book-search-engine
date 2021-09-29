45a664baa54b04d52a5edabe1150125676398858
Jason Viox <jrv04@hotmail.com>
2021-09-29 09:23:51 -0500

    Update middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,7 +18,8 @@ if (process.env.NODE_ENV === 'production') {
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
